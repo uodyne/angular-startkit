18518e55ebe386d98f59b32b635162032d5f92cd
uodyne <uodyne@gmail.com>
2014-09-20 01:23:03 +0200

    Update documents

--- app/controllers.js
+++ app/controllers.js
@@ -1,41 +1,8 @@
-
 app.controller('MainCtrl', function($scope, $routeSegment, loader) {
-
     $scope.$routeSegment = $routeSegment;
     $scope.loader = loader;
-
+    $scope.angularurl = "//ajax.googleapis.com/ajax/libs/angularjs/1.3.0-rc.2/angular.min.js";
     $scope.$on('routeSegmentChange', function() {
         loader.show = false;
     })
 });
-
-app.controller('Section1Ctrl', function($scope, $routeSegment) {
-    
-    $scope.$routeSegment = $routeSegment;
-    $scope.test = { btnClicked: false };
-    $scope.items = [ 1,2,3,4,5 ];
-});
-
-app.controller('Section1ItemCtrl', function($scope, $routeSegment) {
-
-    $scope.$routeSegment = $routeSegment;
-    $scope.item = { id: $routeSegment.$routeParams.id };
-    $scope.test = { textValue: '' };
-});
-
-app.controller('Section2Ctrl', function($scope, $routeSegment) {
-
-    $scope.$routeSegment = $routeSegment;
-    $scope.test = { textValue: '' };
-    $scope.items = [ 1,2,3,4,5,6,7,8,9 ];
-});
-
-app.controller('ErrorCtrl', function($scope, error) {
-    $scope.error = error;
-});
-
-app.controller('SlowDataCtrl', function($scope, data, loader) {
-    loader.show = false;
-    $scope.data = data;
-});
-
---
+++ app/route.js
@@ -0,0 +1,34 @@
+app.config(function($routeSegmentProvider, $routeProvider) {
+
+    $routeSegmentProvider.options.autoLoadTemplates = true;
+
+    $routeSegmentProvider
+        .when('/base', 's1')
+        .when('/base/helloworld',   's1.helloworld')
+        .when('/base/directivas',   's1.directivas')
+        .segment('s1', {
+            templateUrl: 'templates/home.html',
+            controller: 'MainCtrl'})
+            .within()
+                .segment('home', {
+                    default: true,
+                    templateUrl: 'templates/base/home.html'})                
+                .segment('helloworld', {
+                    templateUrl: 'templates/base/helloworld.html'})
+                .segment('directivas', {
+                    templateUrl: 'templates/base/directivas.html'})
+            .up()
+
+
+
+    $routeSegmentProvider
+        .when('/base/directivas/interpolation', 's1.directivas.interpolation')  
+        .when('/base/directivas/ngbind', 's1.directivas.ngbind')  
+        .within('s1')
+            .within('directivas')
+                .segment('interpolation', {default: true, templateUrl: 'templates/base/directivas/interpolation.html'})
+                .segment('ngbind', {templateUrl: 'templates/base/directivas/ngbind.html'})
+
+
+    $routeProvider.otherwise({redirectTo: '/base'}); 
+}) ;
